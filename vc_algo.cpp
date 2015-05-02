0b3e630ed43aebd51d01e6331080c8d88eb4a5d3
jay <ghuryejay@gmail.com>
2015-05-01 23:06:45 -0400

    cleaned code

--- vc_algo.cpp
+++ vc_algo.cpp
@@ -39,56 +39,45 @@ int main(int argc, char* argv[]) {
 	map<string,string> labelmap;
 	string id;
 	string label;
-	int cnt = 1;
 	while (std::getline(ifile, str)) {
-		if(cnt %4 == 0){
+		if(str.find("id") != string::npos){
 			id = str.substr(7);
 		}
-		if((cnt-1)%4 == 0 && cnt != 1) {
+		if(str.find("label") != string::npos) {
 			label = str.substr(10);
 			labelmap[id] = label;
 		}
-		cnt += 1;
 	}
 
-	cout << "Nodes: " << g.nodes().size() << endl;
-	cout << "Edges: " << g.edges().size() << endl;
+	cerr << "Nodes: " << g.nodes().size() << endl;
+	cerr << "Edges: " << g.edges().size() << endl;
 	double delta = 0.1;
 	double epsilon = 0.1;
-	//Betweenness centrality(g);
 	ApproxBetweenness centrality(g, delta, epsilon, 2);
-	//ApproxBetweenness2 centrality(g,100,false);
 	centrality.run();
 	ofstream myfile;
 	filepath = filepath.substr(0,filepath.length()-4);
-	myfile.open(filepath+".repeats");
 	vector<pair<node, double>> nodeRanks(g.nodes().size());
 	nodeRanks = centrality.ranking();
 	vector<double> scores = centrality.scores();
-	vector<double> bc_nonzero;
-	bc_nonzero = scores;
-//	for(int i = 0;i < scores.size();i++){
-//		if(scores.at(i) != 0){
-//			bc_nonzero.push_back(scores.at(i));
-//		}
-//	}
-	cout<<bc_nonzero.size()<<endl;
-	double sum = std::accumulate(bc_nonzero.begin(), bc_nonzero.end(), 0.0);
-	double mean = sum / bc_nonzero.size();
 
-	double sq_sum = std::inner_product(bc_nonzero.begin(), bc_nonzero.end(), bc_nonzero.begin(), 0.0);
-	double stdev = std::sqrt(sq_sum / bc_nonzero.size() - mean * mean);
-	cout<<"Mean: "<<mean<<endl;
-	cout<<"Std Dev: "<<stdev<<endl;
-	cout<<"bound: "<<mean + filterparam*stdev<<endl;
+	double sum = std::accumulate(scores.begin(), scores.end(), 0.0);
+	double mean = sum / scores.size();
+
+	double sq_sum = std::inner_product(scores.begin(), scores.end(), scores.begin(), 0.0);
+	double stdev = std::sqrt(sq_sum / scores.size() - mean * mean);
+	cerr<<"Mean: "<<mean<<endl;
+	cerr<<"Std Dev: "<<stdev<<endl;
+	cerr<<"bound: "<<mean + filterparam*stdev<<endl;
 	double bound = mean + filterparam*stdev;
 	for (int i = 0; i < g.nodes().size(); i++) {
 		if(nodeRanks.at(i).second >= bound){
 			string String = static_cast<ostringstream*>( &(ostringstream() << nodeRanks.at(i).first) )->str();
-			myfile << nodeRanks.at(i).first <<"\t"<< nodeRanks.at(i).second<<"\t"<<labelmap[String]<< endl;
+//			myfile << nodeRanks.at(i).first <<"\t"<< nodeRanks.at(i).second<<"\t"<<labelmap[String]<< endl;
+			cout << nodeRanks.at(i).first <<"\t"<< nodeRanks.at(i).second<<"\t"<<labelmap[String]<< endl;
 		}
 	}
-	cout << "Execution Time = " << (double) (clock() - start) / CLOCKS_PER_SEC
+	cerr << "Execution Time = " << (double) (clock() - start) / CLOCKS_PER_SEC
 			<< endl;
 	return 0;
 }
